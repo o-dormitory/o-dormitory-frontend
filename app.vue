13be0321922955f7491350bef59d5e9d2e00011d
Kirill Sedyshev <agreedeth@gmail.com>
2024-06-26 00:17:38 +0300

    feat(plugins/pinia): add pinia (#318069)

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
+<script setup lang="ts">
+import { useCounterStore } from "./stores/counter";
+
+const counterStore = useCounterStore();
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
+  <div class="counter-be">
+    <button @click="counterStore.decrement">Decrement</button>
+    <b>{{ counterStore.counter }}</b>
+    <button @click="counterStore.increment">Increment</button>
   </div>
 </template>
+
+<style lang="scss">
+.counter-be {
+  display: flex;
+  flex-direction: row;
+}
+</style>
---
+++ stores/counter.ts
@@ -0,0 +1,10 @@
+import { defineStore } from "pinia";
+
+export const useCounterStore = defineStore('counterStore', () => {
+  const counter = ref(0);
+
+  const increment = () => counter.value++;
+  const decrement = () => counter.value--;
+
+  return { counter, increment, decrement };
+})
