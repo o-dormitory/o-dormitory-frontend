0600f8cd1339de55d260b485cf689fee79531177
Kirill Sedyshev <agreedeth@gmail.com>
2024-07-13 15:14:25 +0300

    feat(entities/store): add api to magazine store

--- entities/magazine/store/index.ts
+++ entities/magazine/store/index.ts
@@ -1,10 +1,35 @@
 import _ from 'lodash';
 import { type IMagazine } from '../model';
+import { api } from '~/shared/api';
 
 export const magazineStore = defineStore('magazine', () => {
   const magazines = ref<IMagazine[]>([]);
 
   const magazinesMap = computed(() => _.keyBy(magazines.value, 'uid'));
+
+  const getMagazines = async () => {
+    const allMagazines = await api.magazine!.getAllMagazines();
+
+    magazines.value.push(...allMagazines);
+  };
+
+  const getMagazine = async (uid: string) => {
+    let magazine: IMagazine | null = magazinesMap.value[uid] ?? null;
+
+    if (magazine) {
+      return magazine;
+    }
+
+    magazine = await api.magazine!.getMagazine({ uid });
+
+    if (!magazine) {
+      return null;
+    }
+
+    addMagazineLocally(magazine);
+
+    return magazine;
+  };
 
   const renameMagazineLocally = (uid: string, name: string) => {
     const magazine = magazinesMap.value[uid];
@@ -36,6 +61,8 @@ export const magazineStore = defineStore('magazine', () => {
     magazines,
     magazinesMap,
 
+    getMagazine,
+    getMagazines,
     addMagazineLocally,
     renameMagazineLocally,
     changeAddressLocally,
